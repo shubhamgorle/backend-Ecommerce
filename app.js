2a0f0820559c81d18a1097ddad5b07fa452be793
unknown <shubhamgorle.me@sbjit.edu.in>
2024-08-26 14:52:18 +0530

    deployement commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const cookieparser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileUpload")
+const dotenv = require("dotenv");
+const errorMiddleWare = require("./middleware/error.js")
+ 
+dotenv.config({path:"backend/config/config.env"});
+
+// middlewares
+app.use(express.json());
+app.use(cookieparser())
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(fileUpload())
+
+// Route Import
+const productRoute = require("./routes/productRoute");
+const userRoute = require("./routes/userroute.js");
+const orderRoute = require("./routes/orderRoute.js")
+const paymentRoute = require("./routes/paymentRoute.js")
+app.use("/api/v1", productRoute)
+app.use("/api/v1", userRoute)
+app.use("/api/v1", orderRoute)
+app.use("/api/v1", paymentRoute)
+
+// MiddleWare for error
+app.use(errorMiddleWare)
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,36 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
+const cloudinary = require("cloudinary");
+// Handling Uncaught Exception
+process.on("uncaughtException", (err)=>{
+    console.log(`Error: ${err.message} `);
+    console.log(`Shutting down the server due to uncaught Exception`)
+    process.exit(1);
+})
+
+// config
+dotenv.config({path:"backend/config/config.env"});
+// connect to database
+connectDatabase();
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`server is working on http://localhost/${process.env.PORT}`)
+})
+
+// unhandles Promise Rejection
+
+process.on("unhandledRejection", err=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to unhndles promise rejection`)
+    server.close(()=>{
+        process.exit(1);
+    })
+})

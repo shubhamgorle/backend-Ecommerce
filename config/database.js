9101df95437b8b87be21257f6ab4f13ccef2dea6
unknown <shubhamgorle.me@sbjit.edu.in>
2024-08-29 14:44:08 +0530

    async

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 // process.env.DB_URI 
-const DB_URI = "mongodb+srv://temporory09:ykSZtNRAZs8JU9as@ecommerce.quz7w.mongodb.net/Ecommerce?retryWrites=true&w=majority"
-const connectDatabase = () => {
-    mongoose.connect(DB_URI).then((data) => {
+const DB_URI = process.env.DB_URI || "mongodb+srv://temporory09:ykSZtNRAZs8JU9as@ecommerce.quz7w.mongodb.net/Ecommerce?retryWrites=true&w=majority"
+const connectDatabase = async() => {
+    await mongoose.connect(DB_URI).then((data) => {
         console.log(`mongodb connected with server ${data.connection.host}-${process.env.PORT} `);
     }).catch((error) => {
         console.log(error)

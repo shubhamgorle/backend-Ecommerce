3234708b75c9366ad2df5cdf2caa7173adf5f107
unknown <shubhamgorle.me@sbjit.edu.in>
2024-08-30 14:53:47 +0530

    remove cors

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload")
 const dotenv = require("dotenv");
 const errorMiddleWare = require("./middleware/error.js")
-const cors = require("cors")
-const corsConfig = {
-    origin: "*",
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
-}
+// const cors = require("cors")
+// const corsConfig = {
+//     origin: "*",
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
+// }
 dotenv.config({path:"backend/config/config.env"});
 
 // middlewares
-app.options("*", cors(corsConfig))
-app.use(cors(corsConfig))
+// app.options("*", cors(corsConfig))
+// app.use(cors(corsConfig))
 app.use(express.json());
 app.use(cookieparser())
 app.use(bodyParser.urlencoded({extended:true}));

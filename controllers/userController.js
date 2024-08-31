3a3c78df76aa72f95e879cddc243d2aa7e68ab75
unknown <shubhamgorle.me@sbjit.edu.in>
2024-08-31 15:39:44 +0530

    resetPassUrl updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,8 +79,8 @@ exports.forgotPassword = catchAsyncError(async (req, res, next) => {
   const resetToken = user.getResetPasswordToken();
   await user.save({ validateBeforeSave: false });
 
-  const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`
-  // const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`
+  // const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`
+  const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`
   const message = `your password reset token is temp:- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then, ignore it`
 
   try {
